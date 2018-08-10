813f99ac5a10e1273dc743122b3990e643076992
kimsuran <suran.kim@kt.com>
2018-08-10 14:09:29 +0900

    test

--- test/src/test/java/reservation/service/PrereservationServiceTest.java
+++ test/src/test/java/reservation/service/PrereservationServiceTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.test.context.junit4.SpringRunner;
+import reservation.vo.CheckResponseVO;
+import reservation.vo.CountResponseVO;
+import reservation.vo.EnrollResponseVO;
 
 import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
@@ -17,16 +20,24 @@ public class PrereservationServiceTest {
     private PrereservationService prereservationService;
     @Test
     public void enroll() {
-        prereservationService.enroll(1,1111);
+
+
+        EnrollResponseVO enrollResponseVO = prereservationService.enroll(1,1111);
+        System.out.println(enrollResponseVO.getErrorCode());
+        System.out.println(enrollResponseVO.getErrorMsg());
+        System.out.println(enrollResponseVO.getSeq());
     }
 
-//    @Test
-//    public void count() {
-//        prereservationService.count(1);
-//    }
+    @Test
+    public void count() {
+        CountResponseVO countResponseVO = prereservationService.count(1);
+        System.out.println(countResponseVO.getReservationCount());
+    }
 
     @Test
     public void check() {
-        prereservationService.check(1,1111);
+
+        CheckResponseVO checkResponseVO = prereservationService.check(1,1111);
+        System.out.println(checkResponseVO.getRank1());
     }
 }
